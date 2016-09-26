56749816f3f1c829a1b590ea2346c748692640ec
Andrew Fisher and Stephen Emery <stephen@>
2016-09-26 10:47:25 -0700

    Barebone structure of Book and Patron class.

--- src/main/java/Book.java
+++ src/main/java/Book.java
@@ -3,6 +3,86 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Book {
+  private String title;
+  private String author;
+  private int patronId;
+  private int id;
+
+  public Book(String title, String author, int patronId) {
+    this.title = title;
+    this.author = author;
+    this.patronId = patronId;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public String getAuthor() {
+    return author;
+  }
+
+  public int getId(){
+    return id;
+  }
+
+  public int getPatronId(){
+    return patronId;
+  }
+
+  @Override
+  public boolean equals(Object otherBook){
+    if(!(otherBook instanceof Book)){
+      return false;
+    }else{
+      Book newBook = (Book) otherBook;
+      return this.getName().equals(newBook.getName()) &&  this.getAuthor.equals(newBook.getAuthor());
+    }
+  }
+
+  public void save(){
+    try(Connection con = DB.sql2o.open()){
+      String sql = "INSERT INTO books (title, author, patronId) VALUES (:title, :author, :patronId)";
+      this.id = (int) con.createQuery(sql,true).addParameter("title",this.title).addParameter("author",this.author).addParameter("patronId",this.patronId).executeUpdate().getKey();
+    }
+  }
+  public static List<Book> all(){
+    try(Connection con = DB.sql2o.open()){
+      String sql = "SELECT * FROM books";
+      con.createQuery(sql).executeAndFetch(Book.class);
+    }
+  }
+
+  public static Book find(int id) {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "SELECT * FROM tasks where id=:id";
+      return con.createQuery(sql)
+        .addParameter("id", id)
+        .executeAndFetchFirst(Book.class);
+    }
+  }
+
+  public void update(String title, String author, int patronId) {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "UPDATE tasks SET title = :title, author = :author, patronId = :patronId WHERE id = :id";
+      con.createQuery(sql)
+        .addParameter("title", title)
+        .addParameter("author", author)
+        .addParameter("patronId", patronId)
+        .addParameter("id", id)
+        .executeUpdate();
+    }
+  }
+
+  public void delete() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "DELETE FROM books WHERE id=:id";
+      con.createQuery(sql)
+        .addParameter("id", id)
+        .executeUpdate();
+    }
+  }
+
 
 
 }
--- src/main/java/Patron.java
+++ src/main/java/Patron.java
@@ -3,5 +3,78 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Patron {
+  private String name;
+  private id;
+
+
+  public Patron(String name){
+    this.name = name;
+  }
+
+  public String getName(){
+    return name;
+  }
+
+  public int getId(){
+    return id;
+  }
+
+
+  @Override
+  public boolean equals(Object otherPatron) {
+    if (!(otherPatron instanceof Patron)) {
+      return false;
+    } else {
+      Patron patron = (Patron) otherPatron;
+      return this.getName().equals(patron.getName()) &&
+             this.getId() == patron.getId();
+    }
+  }
+
+  public void save() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "INSERT INTO patrons(name) VALUES (:name)";
+      this.id = (int) con.createQuery(sql, true)
+        .addParameter("name", this.name)
+        .executeUpdate()
+        .getKey();
+    }
+  }
+
+  public List<Patron> all(){
+    try(Connection con = DB.sql2o.open()){
+      String sql = "SELECT * FROM patrons";
+      con.createQuery(sql).executeAndFetch(Patron.class);
+    }
+  }
+
+  public Patron find(int id){
+    try(Connection con = DB.sql2o.open()){
+      String sql = "SELECT * FROM patrons WHERE id=:id";
+      return con.createQuery(sql).addParameter("id",id)
+      .executeAndFetchFirst(Patron.class);
+    }
+  }
+
+  public void update(String name){
+    try(Connection con = DB.sql2o.open()){
+      String sql = "UPDATE patrons SET nane = :name WHERE id =:id";
+      con.createQuery(sql).addParameter("name",name).addParameter("id",id).executeUpdate();
+    }
+  }
+
+  public void delete(){
+    try(Connection con = DB.sql2o.open()){
+      String sql = "DELETE FROM patrons WHERE id=:id";
+      con.createQuery(sql).addParameter("id",id).executeUpdate();
+    }
+  }
+
+  public List<Book> getBooks(){
+    try(Connection con = DB.sql2o.open()){
+      String sql = "SELECT * FROM books WHERE patronId=:id";
+      return con.createQuery(sql).addParameter("id",id).executeAndFetch(Book.class);
+    }
+  }
 
 }
